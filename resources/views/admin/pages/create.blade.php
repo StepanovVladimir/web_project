ee8eeb804c6ac1faed175971759d3ced0d367097
Vladimir Stepanov <vovan.number.one@mail.ru>
2019-03-28 12:39:49 +0400

    Запустил проект

---
+++ resources/views/admin/pages/create.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.admin-index')
+
+@section('title', 'Добавить пост')
+
+@section('content')
+    <div class="col-md-7">
+        {!! Form::open(['route' => 'admin-panel.store']) !!}
+            <div class="form-group">
+                <div class="col-md-3">
+                    {{ Form::label('title', 'Заголовок') }}
+                </div>
+                <div class="col-md-9">
+                    {{ Form::text('title', null, ['class' => 'form-control']) }}
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-3">
+                    {{ Form::label('description', 'Описание') }}
+                </div>
+                <div class="col-md-9">
+                    {{ Form::textarea('description', null, ['class' => 'form-control']) }}
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-3">
+                    {{ Form::label('content', 'Текст') }}
+                </div>
+                <div class="col-md-9">
+                    {{ Form::textarea('content', null, ['class' => 'form-control']) }}
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-9 col-md-offset-3">
+                    {{ Form::submit('Опубликовать', ['class' => 'btn btn-primary']) }}
+                </div>
+            </div>
+        {!! Form::close() !!}
+    </div>
+@endsection
---
+++ resources/views/admin/pages/show.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.admin-index')
+
+@section('title', 'Статья')
+
+@section('content')
+
+    show
+
+@endsection
